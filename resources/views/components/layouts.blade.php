d86b636bf3377b40c56ea88f55208e4986bf0d0f
Kyle <kjrblackman@gmail.com>
2026-02-18 14:00:01 -0400

    Updated Daisy UI VERSION TO 4

--- resources/views/components/layouts.blade.php
+++ resources/views/components/layouts.blade.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+  <link rel="stylesheet" href="{{ asset('build/assets/app-Dbqh7ppH.css') }}"/>
   <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.3/dist/cdn.min.js"></script>
 
   <style>
